a3bf3df6f2173e6b6605bbf82c6cbb057ff42f40
Matan Bibi <matanbibi1432@gmail.com>
2022-11-10 09:55:44 +0200

    changes

--- apps/misterEmail/cmps/email-preview.cmp.js
+++ apps/misterEmail/cmps/email-preview.cmp.js
@@ -2,8 +2,8 @@ export default {
   props:['email'],
   template: `
       <section class="email-preview flex-box" :style="[email.isRead ?{'background-color':'rgb(209, 209, 209)'}:{'background-color':'rgb(230, 230, 230)'}]">
-          <h3 class="emailSender">{{email.sender}}</h3>
-          <h4 class="emailSubject">{{ email.subject }}</h4>
+          <h3 class="emailSender" :style="[email.isRead ?{'font-weight':'100'}:{'font-weight':'bold'}]">{{email.sender}}</h3>
+          <h4 class="emailSubject" :style="[email.isRead ?{'font-weight':'100'}:{'font-weight':'bold'}]">{{ email.subject }} - </h4>
           <p class="emailBody">{{ email.body }}</p>
       </section>
   `
