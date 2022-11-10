61583dabf28bae34a837f82b4c5dc64b18fc3afa
Ronel <2008ronel@gmail.com>
2022-11-10 11:20:25 +0200

    3

--- apps/keep/cmps/note-todo.cmp.js
+++ apps/keep/cmps/note-todo.cmp.js
@@ -3,6 +3,8 @@ export default {
   template: `
     <section>
     <h1>Label: {{keep.info.label}}</h1>
+    <h1>TODO: {{keep.info.todos}}</h1>
+
     </section>
   
   `,
---
+++ apps/keep/cmps/note-txt.cmp.js
@@ -0,0 +1,16 @@
+export default {
+  props: ['keep'],
+  template: `
+      <section>
+      <h1>Text: {{keep.info.txt}}</h1>
+
+      </section>
+
+    `,
+
+  data() {
+    return {};
+  },
+
+  components: {},
+};
