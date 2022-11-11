ca9f163b0ff5b0db875fbfce2d6814b7c2a9d7dd
Matan Bibi <matanbibi1432@gmail.com>
2022-11-11 11:06:22 +0200

    changes

--- views/mister-email.cmp.js
+++ views/mister-email.cmp.js
@@ -1,22 +1,30 @@
 import emailList from '../apps/misterEmail/cmps/email-list.cmp.js'
 import { emailService } from '../apps/misterEmail/services/email-service.js'
 import emailFilter from '../apps/misterEmail/cmps/email-filter.cmp.js'
+import emailForm from '../apps/misterEmail/cmps/email-form.cmp.js'
 import { utilService } from '../services/util.service.js'
 export default {
     template: `
         <section  v-if="emails" class="email-app">
         <h1 class='pageTitle'>Jmail</h1>
-        <email-filter/>
+        <email-filter         
+        @emitStatus = "renderStatus"
+        @emitSendRequest = "composeEmail"/>
         <email-list
         @starEmail='starEmail'
-        :emails = "emails"
+        :emails = "emailsToShow"
         @remove="removeEmail" 
         />
+        <email-form
+        v-if="isFormOpen"
+        @closeForm="isFormOpen=!isFormOpen"/>
         </section>
     `,
     data() {
         return {
             emails: null,
+            emailStatus:'inbox',
+            isFormOpen: false
         }
     },
     created() {
@@ -25,9 +33,13 @@ export default {
     },
     components: {
         emailList,
-        emailFilter
+        emailFilter,
+        emailForm
     },
     methods: {
+        renderStatus(status){
+            this.emailStatus = status
+        },
         starEmail(emailId){
             let emails = utilService.loadFromStorage('Emails')
             const idx = emails.findIndex(email => email.id === emailId)
@@ -36,11 +48,29 @@ export default {
             this.emails = emails
         },
         removeEmail(emailId) {
-            emailService.remove(emailId)
-                .then(() => {
-                    const idx = this.emails.findIndex(email => email.id === emailId)
-                    this.emails.splice(idx, 1)
-                })
+            emailService.query().then(emails => {
+                const idx = emails.findIndex(email => email.id === emailId)
+                emails[idx].status !== 'trash' ? emails[idx].status = 'trash' : emails.splice(idx,1)
+                this.emails = emails
+                utilService.saveToStorage('Emails',this.emails)
+            })
+        },
+        composeEmail(){
+            this.isFormOpen = true
         }
+
+    },
+    computed:{
+        emailsToShow() {
+            let emailArr = this.emails
+            if(this.emailStatus==='inbox'||this.emailStatus==='trash'|| this.emailStatus==='sent') 
+            emailArr = this.emails.filter ( email => this.emailStatus === email.status )
+            else if(this.emailStatus==='starred') emailArr = this.emails.filter ( email => email.isStarred === true )
+            else if(this.emailStatus==='read') emailArr = this.emails.filter ( email => email.isRead === true )
+            else if(this.emailStatus==='unread') emailArr = this.emails.filter ( email => email.isRead === false)
+            // const regex = new RegExp(this.filterBy.title, 'i')
+            return emailArr
+            // .filter(book => regex.test(book.title))
+          }
     }
 }
